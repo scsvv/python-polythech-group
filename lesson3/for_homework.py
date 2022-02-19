09230787d5ddf8fa5624e15b7edcb4cd1122c5c9
Lyshaf <zvat.333333@gmail.com>
2022-02-19 13:34:17 +0200

    lesson 3

---
+++ lesson3/for_homework.py
@@ -0,0 +1,9 @@
+while True:
+    input_number = int(input("Plese write number: "))
+
+    if input_number < 10:
+        print(10)
+
+    is_exit = input("Prees 'y' if u want exit:   ");
+    if is_exit == "y":
+        break
--- lesson3/main.py
+++ lesson3/main.py
@@ -1,2 +1,20 @@
-print("Hello World")
-print("Hello World x2")
+a = 1
+b = 1
+c = 0
+while c < 100:
+    c = a + b
+    if c > 100:
+        break
+    print(c)
+    a = b 
+    b = c
+
+print("------------------------------------")
+a = 1
+b = 1
+c = 0
+for i in range(15):
+    c = a + b
+    print(c)
+    a = b 
+    b = c
